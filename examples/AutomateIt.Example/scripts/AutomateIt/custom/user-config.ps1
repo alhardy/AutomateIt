a6cd0cf190d5baec80891050fbf82ed9b6d234b6
Allan hardy <al_hardy@live.com.au>
2013-03-04 19:28:21 +1100

    removing user config

--- examples/AutomateIt.Example/scripts/AutomateIt/custom/user-config.ps1
+++
@@ -1,5 +0,0 @@
-$global:ProjectRootPath = "E:\Source\MySource-Git\AutomateIt\examples\AutomateIt.Example"
-$global:SolutionRoot = Resolve-Path $ProjectRootPath
-$global:Env = "dev"
-$global:EnvVarPath = Resolve-Path $solutionRoot\env
-$global:SitesToStart = @("AutomateItWeb", "AutomateItWebTwo")
